c4eb1e0dd0d7b2e1aa4e83c3c7e44dd3936404ec
Rick1232134 <141493018+Rick1232134@users.noreply.github.com>
2023-08-05 16:43:56 +0530

    Update nodemcu_wifi_car (1).c

--- nodemcu_wifi_car (1).c
+++ nodemcu_wifi_car (1).c
@@ -1,180 +1,176 @@
-/* Simple WiFi-controlled car on ESP8266 
- * and L9110S motor drive board
- * by Alan Wang
- */
+#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
+#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
+#define IN_1  15          // L298N in1 motors Rightx          GPIO15(D8)
+#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
+#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
+#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
 
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-// WiFi settings
-#define SSID_AP            "ESP8266_WiFi_Car" // AP server name
-#define PASSWORD_AP        "12345678"         // AP server password
+String command;             //String to store app command state.
+int speedCar = 800;         // 400 - 1023.
+int speed_Coeff = 3;
 
-// motor settings
-#define RIGHT_MOTOR_PIN1  4                   // pin 1 of right motor (D2)
-#define RIGHT_MOTOR_PIN2  5                   // pin 2 of right motor (D1)
-#define LEFT_MOTOR_PIN1   12                  // pin 1 of left motor (D6)
-#define LEFT_MOTOR_PIN2   14                  // pin 2 of left motor (D5)
-#define RIGHT_MOTOR_SPEED 1023                // speed for right motor (0-1023)
-#define LEFT_MOTOR_SPEED  1023                // speed for left motor (0-1023)
-#define LED_PIN           2                   // built-in LED (D4)
-
-IPAddress local_ip(192, 168, 4, 1);           //IP for AP mode
-IPAddress gateway(192, 168, 100, 1);          //IP for AP mode
-IPAddress subnet(255, 255, 255, 0);           //IP for AP mode
+const char* ssid = "Electroniclinic";
 ESP8266WebServer server(80);
 
-int car_mode = 0;                             // set car drive mode (0 = stop)
-
-// initialize
 void setup() {
+ 
+ pinMode(ENA, OUTPUT);
+ pinMode(ENB, OUTPUT);  
+ pinMode(IN_1, OUTPUT);
+ pinMode(IN_2, OUTPUT);
+ pinMode(IN_3, OUTPUT);
+ pinMode(IN_4, OUTPUT); 
   
   Serial.begin(115200);
-  Serial.println("ESP8266 Wifi Car");
-  pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
-  pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
-  pinMode(LEFT_MOTOR_PIN1, OUTPUT);
-  pinMode(LEFT_MOTOR_PIN2, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
-  car_control(); // stop the car
+  
+// Connecting WiFi
 
   WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  WiFi.softAP(SSID_AP, PASSWORD_AP);
+  WiFi.softAP(ssid);
 
-  // setup web server to handle specific HTTP requests
-  server.on("/", HTTP_GET, handle_OnConnect);
-  server.on("/forward", HTTP_GET, handle_forward);
-  server.on("/backward", HTTP_GET, handle_backward);
-  server.on("/left", HTTP_GET, handle_left);
-  server.on("/right", HTTP_GET, handle_right);
-  server.on("/stop", HTTP_GET, handle_stop);
-  server.onNotFound(handle_NotFound);
-
-  //start server
-  server.begin();
-  Serial.println("ESP8266 car server started.");
-  digitalWrite(LED_PIN, LOW);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+ 
+ // Starting WEB-server 
+     server.on ( "/", HTTP_handleRoot );
+     server.onNotFound ( HTTP_handleRoot );
+     server.begin();    
 }
 
-// handle HTTP requests and control car
-void loop() { 
-  server.handleClient();
-  car_control();
-}
+void goAhead(){ 
 
-// HTTP request: on connect
-void handle_OnConnect() {
-  car_mode = 0;
-  Serial.println("Client connected");
-  server.send(200, "text/html", SendHTML());
-}
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
 
-// HTTP request: stop car
-void handle_stop() {
-  car_mode = 0;
-  Serial.println("Stopped");
-  server.send(200, "text/html", SendHTML());
-}
-
-// HTTP request: go forward
-void handle_forward() {
-  car_mode = 1;
-  Serial.println("Go forward...");
-  server.send(200, "text/html", SendHTML());
-}
-
-// HTTP request: go backward
-void handle_backward() {
-  car_mode = 2;
-  Serial.println("Go backward...");
-  server.send(200, "text/html", SendHTML());
-}
-
-// HTTP request: turn left
-void handle_left() {
-  car_mode = 3;
-  Serial.println("Turn left...");
-  server.send(200, "text/html", SendHTML());
-}
-
-// HTTP request: turn right
-void handle_right() {
-  car_mode = 4;
-  Serial.println("Turn right...");
-  server.send(200, "text/html", SendHTML());
-}
-
-// HTTP request: other
-void handle_NotFound() {
-  car_mode = 0;
-  Serial.println("Page error");
-  server.send(404, "text/plain", "Not found");
-}
-
-// control car movement
-void car_control() {
-  switch (car_mode) {
-    case 0: // stop car
-      digitalWrite(RIGHT_MOTOR_PIN1, LOW);
-      digitalWrite(RIGHT_MOTOR_PIN2, LOW);
-      digitalWrite(LEFT_MOTOR_PIN1, LOW);
-      digitalWrite(LEFT_MOTOR_PIN2, LOW);
-      break;
-    case 1: // go forward
-      analogWrite(RIGHT_MOTOR_PIN1, RIGHT_MOTOR_SPEED);
-      digitalWrite(RIGHT_MOTOR_PIN2, LOW);
-      analogWrite(LEFT_MOTOR_PIN1, LEFT_MOTOR_SPEED);
-      digitalWrite(LEFT_MOTOR_PIN2, LOW);
-      break;
-    case 2: // go backward
-      digitalWrite(RIGHT_MOTOR_PIN1, LOW);
-      analogWrite(RIGHT_MOTOR_PIN2, RIGHT_MOTOR_SPEED);
-      digitalWrite(LEFT_MOTOR_PIN1, LOW);
-      analogWrite(LEFT_MOTOR_PIN2, LEFT_MOTOR_SPEED);
-      break;
-    case 3: // turn left
-      analogWrite(RIGHT_MOTOR_PIN1, RIGHT_MOTOR_SPEED);
-      digitalWrite(RIGHT_MOTOR_PIN2, LOW);
-      digitalWrite(LEFT_MOTOR_PIN1, LOW);
-      analogWrite(LEFT_MOTOR_PIN2, LEFT_MOTOR_SPEED);
-      break;
-    case 4: // turn right
-      digitalWrite(RIGHT_MOTOR_PIN1, LOW);
-      analogWrite(RIGHT_MOTOR_PIN2, RIGHT_MOTOR_SPEED);
-      analogWrite(LEFT_MOTOR_PIN1, LEFT_MOTOR_SPEED);
-      digitalWrite(LEFT_MOTOR_PIN2, LOW);
-
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
   }
+
+void goBack(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goLeft(){
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goAheadRight(){
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar/speed_Coeff);
+ 
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+   }
+
+void goAheadLeft(){
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void goBackRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar/speed_Coeff);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBackLeft(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void stopRobot(){  
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+ }
+
+void loop() {
+    server.handleClient();
+    
+      command = server.arg("State");
+      if (command == "F") goAhead();
+      else if (command == "B") goBack();
+      else if (command == "L") goLeft();
+      else if (command == "R") goRight();
+      else if (command == "I") goAheadRight();
+      else if (command == "G") goAheadLeft();
+      else if (command == "J") goBackRight();
+      else if (command == "H") goBackLeft();
+      else if (command == "0") speedCar = 400;
+      else if (command == "1") speedCar = 470;
+      else if (command == "2") speedCar = 540;
+      else if (command == "3") speedCar = 610;
+      else if (command == "4") speedCar = 680;
+      else if (command == "5") speedCar = 750;
+      else if (command == "6") speedCar = 820;
+      else if (command == "7") speedCar = 890;
+      else if (command == "8") speedCar = 960;
+      else if (command == "9") speedCar = 1023;
+      else if (command == "S") stopRobot();
 }
 
-// output HTML web page for user
-String SendHTML() {
-  String html = "<!DOCTYPE html>";
-  html += "<html>";
-  html += "<head>";
-  html += "<title>ESP8266 WiFi Car</title>";
-  html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-  html += "<style>body {background-color: Moccasin;} h1 {color: SaddleBrown;} h2 {color: Olive;}</style>";
-  html += "</head>";
-  html += "<body>";
-  html += "<div align=\"center\">";
-  html += "<h1>ESP8266 WiFi Car</h1>";
-  html += "<h2>Press \"stop\" after the server has been reset</h2>";
-  html += "<br>\n";
-  html += "<form method=\"GET\">";
-  html += "<input type=\"button\" value=\"Go forward\" onclick=\"window.location.href='/forward'\">";
-  html += "<br><br>\n";
-  html += "<input type=\"button\" value=\"Go backward\" onclick=\"window.location.href='/backward'\">";
-  html += "<br><br>\n";
-  html += "<input type=\"button\" value=\"Turn left\" onclick=\"window.location.href='/left'\">";
-  html += "<br><br>\n";
-  html += "<input type=\"button\" value=\"Turn right\" onclick=\"window.location.href='/right'\">";
-  html += "<br><br>\n";
-  html += "<input type=\"button\" value=\"Car stop\" onclick=\"window.location.href='/stop'\">";
-  html += "</form>\n";
-  html += "</div>\n";
-  html += "</body>\n";
-  html += "</html>\n";
-  return html;
+void HTTP_handleRoot(void) {
+
+if( server.hasArg("State") ){
+       Serial.println(server.arg("State"));
+  }
+  server.send ( 200, "text/html", "" );
+  delay(1);
 }
